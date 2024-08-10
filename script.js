871fecd9f408326a5d6cd2bca19cab2dd941cc69
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-08-10 21:57:18 +0600

    k

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const apiKey = 'd465350e295a4b9aa21ebf0e5252c1ec'
+
+const blogContainer = document.getElementById("blog-container");
+
+function fetchRandomNews(){
+    try{
+
+    }catch(){
+        
+    }
+}
