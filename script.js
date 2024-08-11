109ff5d3204ad2f8fd50553ae7585c0ce6c248ca
Khadiza-Akter-Developer <khadizaakter44555@gmail.com>
2024-08-11 20:15:40 +0600

    p

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ const apiKey = 'd465350e295a4b9aa21ebf0e5252c1ec'
 
 const blogContainer = document.getElementById("blog-container");
 
-function fetchRandomNews(){
-    try{
-
-    } catch(){
-
+async function fetchRandomNews() {
+    try {
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.error("Error fetching random news", error);
+        return [];
     }
 }
+
+function displayBlogs(articles) {
+    blogContainer.innerHTML = " "
+    articles.forEach((article) =>{
+        const blogCard = document.createElement("div");
+        blogCard.classList.add("blog-card");
+        const img = document.createElement("img");
+        img.src = article.urlToImage;
+        img.alt = article.title;
+        const title = document.createElement("h2");
+        title.textContent = article.title;
+        const description = document.createElement("p");
+        description.textContent = article.description;
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogContainer.appendChild(blogCard);
+    })
+}
+
+(async () => {
+    try {
+        const articles = await fetchRandomNews();
+        displayBlogs(articles);
+    } catch (error) {
+        console.error("Error fetching random news", error);
+    }
+})();
